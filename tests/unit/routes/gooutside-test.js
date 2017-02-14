0efc090fd18851ad98e04e29c86973c8b9d4b614
jjwang123 <jesswaaang@gmail.com>
2017-02-14 10:58:19 -0800

    details

---
+++ tests/unit/routes/gooutside-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:gooutside', 'Unit | Route | gooutside', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
